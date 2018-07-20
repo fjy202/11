1f6f5b4863fe57cd8ec3387ad195eff96d2cd43d
fjy202 <40914915+fjy202@users.noreply.github.com>
2018-07-20 14:06:35 +0800

    Add files via upload

---
+++ mkurl.py
@@ -0,0 +1,23 @@
+#-*- coding:utf-8 -*-
+import os
+from os import path
+import urllib.request
+import time
+def mk_link(url):
+    print("url需带协议。")
+    try:
+        urllib.request.urlopen (url).read ()
+    except:
+        print("错误：无法访问。")
+        exit()
+    f=open(r'C:\Users\Administrator\Desktop\\'+input("名称:")+'.url','w')
+    f.write('[{000214A0-0000-0000-C000-000000000046}]')
+    f.write('\n')
+    f.write('Prop3=19,4')
+    f.write ('\n')
+    f.write('[InternetShortcut]')
+    f.write ('\n')
+    f.write("URL="+url)
+    f.write ('\n')
+    f.write('IDList=')
+mk_link(input("url:"))
